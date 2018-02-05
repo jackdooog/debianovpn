f2737dbd7fdcf1079fbe2af5b8fbdb09443c8b35
jackdooog <32920238+jackdooog@users.noreply.github.com>
2018-02-06 04:09:34 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+clear
+
+# get the VPS IP
+#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+#myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+
+flag=0
+
+echo
+
+	#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+	#if [ "$MYIP" = "" ]; then
+		#MYIP=$(wget -qO- ipv4.icanhazip.com)
+	#fi
+
+	clear
+
+	#echo "--------------- Selamat datang di Server - IP: $MYIP ---------------"
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	#cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+	#cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+	#freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+	#tram=$( free -m | awk 'NR==2 {print $2}' )
+	#swap=$( free -m | awk 'NR==4 {print $2}' )
+	#up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+
+	#echo -e "\e[032;1mCPU model:\e[0m $cname"
+	#echo -e "\e[032;1mNumber of cores:\e[0m $cores"
+	#echo -e "\e[032;1mCPU frequency:\e[0m $freq MHz"
+	#echo -e "\e[032;1mTotal amount of ram:\e[0m $tram MB"
+	#echo -e "\e[032;1mTotal amount of swap:\e[0m $swap MB"
+	#echo -e "\e[032;1mSystem uptime:\e[0m $up"
+lolcat -F 0.3 -S 0 /usr/bin/bannermenu
+echo "                           Server: $MYIP" | lolcat
+date +"                           %A, %d-%m-%Y" | lolcat
+date +"                                   %H:%M:%S %Z" | lolcat
+echo ""
+echo ""
+PS3='Silahkan ketik nomor pilihan anda lalu tekan ENTER: '
+options=("Buat User SSH/OVPN" "Buat User SSH/OVPN Trial" "Perbarui User" "Ganti Password User SSH/OVPN" "Semua User Dan Tanggal Kadaluarsa" "Hapus User" "Buat User PPTP VPN" "Monitoring User Dan Tendang" "Monitor User Login" "Daftar User Aktif" "Daftar User Kadaluarsa" "Disable User Kadaluarsa" "Hapus User Kadaluarsa" "Banned User" "Unbanned User" "Penggunaan Ram" "Speedtest" "Benchmark" "Manual Kill Multi Login" "(ON) Auto Kill Multi Login" "(OFF) Auto Kill Multi Login" "Ganti Password VPS" "Bersihkan Cache Ram Manual" "Edit Banner Login" "Edit Banner Menu" "Lihat Lokasi User" "Restart Webmin" "Restart Server VPS" "Restart Dropbear" "Restart OpenSSH" "Restart Squid3" "Restart OpenVPN" "Ganti Port OpenSSH" "Ganti Port Dropbear" "Ganti Port Squid3" "Ganti Port OpenVPN" "Update Script VPS" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+1)
+clear
+user-add
+exit
+;;
+2)
+clear
+user-add-vpn
+exit
+;;
+3)
+clear
+user-banned
+exit
+;;
+4)
+clear
+unbanned-user
+exit
+;;
+5)
+clear
+user-gen
+exit
+;;
+6)
+clear
+user-pass
+exit
+;;
+7)
+clear
+delete-user-expire
+exit
+;;
+18)
+clear
+ps-mem
+exit
+;;
+19)
+clear
+speedtest --share
+exit
+;;
+20)
+clear
+benchmark
+exit
+;;
+21)
+clear
+wget https://raw.githubusercontent.com/brantbell/wulandari/srie/update.sh
+chmod +x update.sh
+./update.sh
+exit
+;;
+22)
+clear
+reboot
+exit
+;;
+x)
+clear
+exit
+;;
+esac
+done
+
+cd ~/
